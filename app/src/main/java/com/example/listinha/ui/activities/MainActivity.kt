2fa0a1ccde8c59d0fba709a29406593f614d4f6d
uvsl <ugovslima@gmail.com>
2025-04-02 21:56:10 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/listinha/ui/activities/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.listinha.ui.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.listinha.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
